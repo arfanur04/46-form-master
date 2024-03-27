08b881419b7d12c9b9d9b3e1dd91f8aeb8478ff5
arfanur04 <arfanur04@gmail.com>
2024-03-27 21:53:05 +0600

    46-7

--- src/components/Uncle/Uncle.jsx
+++ src/components/Uncle/Uncle.jsx
@@ -1,12 +1,15 @@
 import Cousin from "../Cousin/Cousin";
 
-const Uncle = () => {
+const Uncle = ({ asset }) => {
 	return (
 		<div>
 			<h2>
 				Uncle
 				<section className="flex">
-					<Cousin name={"Rafsan"}></Cousin>
+					<Cousin
+						name={"Rafsan"}
+						asset={asset}
+					></Cousin>
 					<Cousin name={"Sohana"}></Cousin>
 				</section>
 			</h2>
